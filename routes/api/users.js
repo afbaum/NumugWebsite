e6d7f537238d20182927539bf85e8255ff8e141c
Amanda <amanda.baum01@gmail.com>
2019-09-01 18:29:02 -0400

    fixing routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult } = require('express-validator');
 
-// @route   GET api/user
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.json({msg: "User works"}));
 
-// @route   GET api/users/test
+// @route   POST api/users/
 // @desc    Tests User route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: 'User Works'}));
+router.post('/', [
+  check('email').isEmail(), check('password').isLength({ min:8 })
+](req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array()})
+  }
+
+  User.create({
+    
+  })
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Body Parser middleware
+app.use(express.json({extended: false}));
+
 // Connect DAtabase
-connectDB();
+// connectDB();
+
+// DB Config local
+const db = 'mongodb://localhost/numugdev';
+
+// Connect to  MongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('API running'));
 
+
 // Define Routes
-app.use('api/users', require('./routes/api/users'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 8080;
 
